1f2cd1d3bb12344c157b7f98d5fd6126eb353eb2
cbcoutinho <chrisbcoutinho@gmail.com>
2017-12-10 02:00:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learn PostgreSQL using 'The Complete SQL Bootcamp'
+
+This is a repository that contains work related to the sql course I'm
+taking to learn postgresql.
---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import psycopg2
+
+def get_conn(dbname=None, dbuser=None, dbpass=None):
+    try:
+        conn = psycopg2.connect(dbname=dbname,
+                                user=dbuser,
+                                password=dbpass)
+
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
+    except Exception as e:
+        print(e)
+        return None
+
+    return conn
+
+if __name__ == '__main__':
+    with get_conn(dbname='dvdtwo', dbuser='chris') as conn:
+        df = pd.read_sql('select * from film', conn)
+
+        print(df)
+
